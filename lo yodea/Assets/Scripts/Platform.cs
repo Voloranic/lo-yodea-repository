5434ca2093dc35b0b27ed27f1883ed14ba6051ea
amir-m-shulman <amir.shulman08@gmail.com>
2026-01-21 09:32:15 +0200

    improve platform

--- lo yodea/Assets/Scripts/Platform.cs
+++ lo yodea/Assets/Scripts/Platform.cs
@@ -4,16 +4,24 @@ public class Platform : MonoBehaviour
 {
     BoxCollider2D Bcollider;
     [SerializeField] private float activeDelay;
+    private LineRenderer lineRenderer;
+    [SerializeField]Transform point;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Bcollider = this.gameObject.GetComponentInParent<BoxCollider2D>();
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 2;
+        lineRenderer.startWidth = 0.02f;
+        lineRenderer.endWidth = 0.01f;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, point.position);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
